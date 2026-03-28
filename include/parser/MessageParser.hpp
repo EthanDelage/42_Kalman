be46447a4b8bbaa1c26caa4f6d497837a6996568
Ethan Delage <ethandelage04@gmail.com>
2026-03-28 17:25:35 +0100

    feat: implement MessageParser class

--- include/parser/MessageParser.hpp
+++ include/parser/MessageParser.hpp
@@ -2,6 +2,9 @@
 #define KALMAN_MESSAGEPARSER_HPP
 #include "EventParser.hpp"
 
+#define MSG_START_STR "MSG_START"
+#define MSG_END_STR "MSG_END"
+
 class MessageParser {
 public:
   enum class MessageType {
@@ -11,13 +14,16 @@ public:
     Undefined,
   };
 
+  MessageParser();
+
   MessageType parse(std::istream &str);
 
-  std::vector<EventParser::event_t> &get_events() const;
+  std::vector<EventParser::event_t> get_events() const;
   void clear_events();
 
 private:
   std::vector<EventParser::event_t> _events;
+  bool _message_started;
 };
 
 #endif // KALMAN_MESSAGEPARSER_HPP
