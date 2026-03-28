77df9913594b2f1d26e137336f8fe5fdec35de2d
mururoahh <ferraudhugo@gmail.com>
2026-03-28 16:34:12 +0100

    feat: add udp_socket class

--- include/UdpSocket.hpp
+++ include/UdpSocket.hpp
@@ -1,17 +1,17 @@
 #ifndef KALMAN_UDPSOCKET_HPP
 #define KALMAN_UDPSOCKET_HPP
-#include <array>
+#include <string>
 
 #define BUFFER_SIZE 2048
 
 class UdpSocket {
 public:
-  std::string recv();                // TODO: throw exception
-  void send(const std::string &str); // TODO: throw exception
+  UdpSocket();
+  std::string recv() const;
+  void send(const std::string &str) const;
 
 private:
   int _socket_fd;
-  std::array<char, BUFFER_SIZE> _buffer; // TODO: move to recv() method
 };
 
 #endif // KALMAN_UDPSOCKET_HPP
